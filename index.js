18caca867019aa1e3033c089e73abfcd45b66a98
jjjjjbb <94190954+jjjjjbb@users.noreply.github.com>
2022-12-06 20:46:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ const routes = require('./routes/routes');
 
 app.use('/api', routes)
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000), () => {
     console.log(`Server Started at ${3000}`)
-})
+}
