9c2b8868e9ee59ceb7a4af012843b531078abc27
jjjjjbb <94190954+jjjjjbb@users.noreply.github.com>
2023-01-06 00:27:24 +0800

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,70 +3,6 @@ const express = require('express');
 const Model = require('../models/model');
 const router = express.Router();
 
-//Post Method
-router.post('/post', async (req, res) => {
-    const data = new Model({
-        id : req.body.id,
-        question: req.body.question,
-        option1: req.body.option1,
-        option2: req.body.option2,
-        option3: req.body.option3,
-        option4: req.body.option4,
-        correct: req.body.correct,
-        answered:req.body.answered,
-    })
-
-    try {
-        const dataToSave = await data.save();
-        res.status(200).json(dataToSave)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
-
-router.post('/postANS', async (req, res) => {
-    const today = new Date();
-    const date = today.getDate();
-    try {
-        Model.find({id: date.toString()}, async (error, results) => {
-            if(error)
-            {
-              res.status(400).json({error})
-              return false
-            }
-            if(results[0].answer == req.body.option){
-
-                Model.updateOne(
-                    {id:date.toString()}, 
-                    {answered: "Y",correct:"true",herAnswer:req.body.option}, function (err, docs) {
-                    if (err){
-                        console.log(err)
-                    }
-                    else{
-                        console.log("Updated Docs : ", docs);
-                    }
-                });
-                res.json("correct!");
-            }else{
-                Model.updateOne(
-                    {id:date.toString()}, 
-                    {answered: "Y",correct:"false",herAnswer:req.body.option}, function (err, docs) {
-                    if (err){
-                        console.log(err)
-                    }
-                    else{
-                        console.log("Updated Docs : ", docs);
-                    }
-                });
-                res.json("false");
-            }
-          })        
-        }
-    catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
 
 //Get all Method
 router.get('/getAll', async (req, res) => {
@@ -79,10 +15,11 @@ router.get('/getAll', async (req, res) => {
     }
 })
 
-//Get by ID Method
-router.get('/getOne/:id', async (req, res) => {
+
+router.get('/getDiary', async (req, res) => {
     try {
-        const data = await Model.findById(req.params.id);
+        const date = req.query["date"];
+        const data = await Model.find({ date: date});
         res.json(data)
     }
     catch (error) {
@@ -90,34 +27,29 @@ router.get('/getOne/:id', async (req, res) => {
     }
 })
 
-//Update by ID Method
-router.patch('/update/:id', async (req, res) => {
+router.post('/postDiary', async (req, res) => {
     try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
 
-        const result = await Model.findByIdAndUpdate(
-            id, updatedData, options
-        )
+        Model.updateOne(
+            {date:req.body.date}, 
+            {answered: "Y",herDiary:req.body.diary}, function (err, docs) {
+            if (err){
+                console.log(err)
+            }
+            else{
+                console.log("Updated Docs : ", docs);
+            }
+        })
 
-        res.send(result)
+        console.log(req.body);
+    
+        res.json(data)
     }
     catch (error) {
         res.status(500).json({ message: error.message })
     }
 })
 
-//Delete by ID Method
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await Model.findByIdAndDelete(id)
-        res.send(`Document with ${data.name} has been deleted..`)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+
 
 module.exports = router;
